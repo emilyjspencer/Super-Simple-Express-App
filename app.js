f2952681c1f7e5dc2da482e0d99736711f202145
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-18 17:03:14 +0100

    Initializes Express app. Adds homepage route and route for a route that responds to all requests

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const app = express(); 
+
+
+
+app.get("/", function(req, res) {
+    res.send("Welcome to the Homepage")
+});
+
+
+app.get("*", function(req, res) {
+    res.send("Page not found")
+})
+
+app.listen(3000, function() {
+    console.log("Server listening on port 3000 for incoming requests")
+});
