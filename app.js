845af3d4426e1be5d81835e7bc154efa06722a01
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-18 17:16:46 +0100

    Adds place route and using route params so that the user can enter a country and city in the url and will see their submissions on the page

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.get("/", function(req, res) {
     res.send("Welcome to the Homepage")
 });
 
+app.get("/place/:country/:city", function(req, res) {
+    var countryparam = req.params.country;
+    var cityparam = req.params.city;
+    console.log(req.params)
+    res.send(cityparam.charAt(0).toUpperCase() + cityparam.slice(1) + " is a city in " + countryparam.charAt(0).toUpperCase() + countryparam.slice(1))
+ });
 
 app.get("*", function(req, res) {
     res.send("Page not found")
